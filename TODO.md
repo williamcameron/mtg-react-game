72dcfb8f27530655e7d280fe0c5830b781a6ec13
William Cameron <william.cameron@mtcmedia.co.uk>
2018-08-22 17:46:20 +0100

    tidy up hand and css

--- TODO.md
+++ TODO.md
@@ -1,3 +1,8 @@
 * create a Game / App component
 * create hand and deck components
 * create a card object
+
+* add a store
+* add a dispatch
+* add reducers
+* overall, pass cards from deck to hand
--- src/features/game/index.js
+++ src/features/game/index.js
@@ -7,6 +7,13 @@ import './Game.css';
 
 class Game extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            'cards': {}
+        };
+    }
+
     render() {
        return (
        <div className="Game">
